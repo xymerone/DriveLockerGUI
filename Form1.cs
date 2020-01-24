e7318d42714a26c525988555ce6a4c6461de1f1e
xymerone <54781590+xymerone@users.noreply.github.com>
2020-01-24 19:17:18 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DriveLockerGUI
+{
+    public partial class Form1 : Form
+    {
+        
+        public Thread Lock; // Потік перевірки ключа
+        public bool Toggle; // Перемикач для кнопки старт\стоп
+        public Form1()
+        {
+            InitializeComponent();
+            RefreshCombo();
+            Lock = new Thread(new ThreadStart(Program.Locker));
+            Toggle = false;
+            
+        }
+
+        public void StartConfig()
+        {
+            
+            if (Convert.ToBoolean(Program.options.GetValue("HideStart")))
+            {
+                ToTray();
+                checkBox2.Checked = true;
+            }
+            if (Convert.ToBoolean(Program.options.GetValue("Autorun")))
+            {
+                checkBox1.Checked = true;
+            }
+            if (Convert.ToBoolean(Program.options.GetValue("Autostart")))
+            {
+                checkBox3.Checked = true;
+                Lock.Start();
+                Toggle = true;
+                button4.Text = "Стоп";
+                ToTray();
+            }
+        }
+
+        public void RefreshCombo()
+        {
+            List<string> Rem = Program.GetFlashka();
+            comboBox1.Items.Clear();
+            if (Rem.Count == 0)
+            {
+                comboBox1.Items.Add("Немає флешки");
+            }
+            else
+            {
+                foreach (string s in Rem)
+                {
+                    comboBox1.Items.Add(s);
+                }
+            }
+        }
+
+        public void AutoRefreshDevice()
+        {
+            while( true)
+            {
+                Thread.Sleep(2000);
+                RefreshCombo();
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            
+            if (Toggle)
+            {
+                Lock.Abort();
+            }
+            Program.options.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+            {
+                log.Text = "Вкажіть сикрет ключа!!!";
+                return;
+            }
+            string FileKey = (string)comboBox1.SelectedItem + Program.options.GetValue("NameKeyFile").ToString();
+            if (!File.Exists(FileKey))
+            {
+                string hs = Program.GetMd5Hash(textBox1.Text + Program.sald);
+                textBox1.Text = "";
+                File.WriteAllText(FileKey, hs);
+                File.SetAttributes(FileKey, FileAttributes.Hidden);
+                File.SetAttributes(FileKey, FileAttributes.ReadOnly);
+                Program.options.SetValue("Hash", hs);
+                log.Text = "Ключ " + FileKey + " створено!";
+                if (!button4.Enabled)
+                {
+                    button4.Enabled = true;
+                    log1.Text = "";
+                }
+            }
+            else
+            {
+                log.Text = "Ключ вже створено";
+            }
+        }
+
+        public void ToTray()
+        {
+            this.ShowInTaskbar = false;
+            this.Hide();
+            notifyIcon1.Visible = true;
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            
+                this.WindowState = FormWindowState.Normal;
+                this.ShowInTaskbar = true;
+            this.Show();
+                notifyIcon1.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ToTray();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                Program.AddToAutorun();
+                Program.options.SetValue("Autorun", 1);
+            }
+            else
+            {
+                Program.AddToAutorun(true);
+                Program.options.SetValue("Autorun", 0);
+            }
+            if (checkBox2.Checked)
+            {
+                Program.options.SetValue("HideStart", 1);
+            }
+            else
+            {
+                Program.options.SetValue("HideStart", 0);
+            }
+            if (checkBox3.Checked)
+            {
+                Program.options.SetValue("Autostart", 1);
+            }
+            else
+            {
+                Program.options.SetValue("Autostart", 0);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartConfig();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (Program.options.GetValue("Hash").ToString().Length == 0)
+            {
+                log1.Text = "Флешка-ключ не створена!!!";
+                button4.Enabled = false;
+                return;
+            }
+            if (Toggle)
+            {
+                Lock.Abort();
+                Lock = new Thread(new ThreadStart(Program.Locker));
+                button4.Text = "Старт";
+                Toggle = false;
+            }
+            else
+            {
+                Toggle = true;
+                button4.Text = "Стоп";
+                Lock.Start();
+            }
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Куда клікаєш?? Тут нічого неамє!!!");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            RefreshCombo();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using Microsoft.Win32;
+using System.Windows.Forms;
+
+namespace DriveLockerGUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        static string COnfigKeyName = "DriveLocker"; 
+        public static string sald = "AypiaCgw#N";
+        public static RegistryKey options;
+        [STAThread]
+        
+        static void Main()
+        {
+            options = GetRegistryOpt();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            
+        }
+        public static RegistryKey GetRegistryOpt()
+        {
+            if (!ExistsConfigRegistry())
+            {
+                RegistryKey currentUserKey = Registry.CurrentUser
+                .OpenSubKey(@"Software", true);
+                RegistryKey DLKey = currentUserKey.CreateSubKey(COnfigKeyName);
+                DLKey.SetValue("AutorunName", "DriveLockerGUI");
+                DLKey.SetValue("Autorun", 0);
+                DLKey.SetValue("Autostart", 0);
+                DLKey.SetValue("HideStart", 0);
+                DLKey.SetValue("TimeoutBlock", 3500);
+                DLKey.SetValue("Hash", "");
+                DLKey.SetValue("NameKeyFile", "DriveLocker.key");
+                DLKey.Close();
+                return Registry.CurrentUser
+                    .OpenSubKey(@"Software\" + COnfigKeyName, true);
+            }
+            else
+            {
+                return Registry.CurrentUser
+                    .OpenSubKey(@"Software\" + COnfigKeyName, true);
+            }
+            
+
+        }
+        static bool ExistsConfigRegistry()
+        {
+            RegistryKey currentUserKey = Registry.CurrentUser
+                .OpenSubKey(@"Software");
+            foreach(string s in currentUserKey.GetSubKeyNames())
+            {
+                if (s == COnfigKeyName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        public static List<string> GetFlashka()
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            List<string> Flash = new List<string>(1);
+            foreach (DriveInfo d in allDrives)
+            {
+                if (d.DriveType.ToString() != "Removable") // Только флешки
+                {
+                    continue;
+                }
+                Flash.Add(d.Name);
+            }
+            return Flash;
+        }
+        
+        public static string GetMd5Hash(string input)
+        {
+            MD5 md5Hash = MD5.Create();
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+        public static void AddToAutorun(bool delete = false)
+        {
+            string FileRoot = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+            string name = options.GetValue("AutorunName").ToString();
+            RegistryKey AutoRun = Registry.CurrentUser
+                .OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+            if (delete)
+            {
+                AutoRun.DeleteValue(name, false);
+                AutoRun.Close();
+                return;
+            }
+            foreach (string RegKey in AutoRun.GetValueNames())
+            {
+                if (RegKey == name)
+                {
+                    return;
+                }
+            }
+            AutoRun.SetValue(name, FileRoot);
+            AutoRun.Close();
+        }
+        public static void Locker()
+        {
+            string FileName = options.GetValue("NameKeyFile").ToString();
+            string key = options.GetValue("Hash").ToString();
+            int timeout = Convert.ToInt32(options.GetValue("TimeoutBlock"));
+            while (true)
+            {
+                List<string> drives = GetFlashka();
+                if (drives.Count == 0)
+                {
+                    System.Diagnostics.Process.Start(@"C:\WINDOWS\system32\rundll32.exe", "user32.dll,LockWorkStation");
+                    Thread.Sleep(timeout);
+                    continue;
+                }
+                bool Verif = false;
+                foreach (string s in drives)
+                {
+                    string fi = s + FileName;
+                    if (File.Exists(fi) && File.ReadAllText(fi) == key)
+                    {
+                        Verif = true;
+                        break;
+                    }
+                }
+                if (!Verif)
+                {
+                    System.Diagnostics.Process.Start(@"C:\WINDOWS\system32\rundll32.exe", "user32.dll,LockWorkStation");
+                }
+                Thread.Sleep(timeout);
+            }
+        }
+
+    }
+}
